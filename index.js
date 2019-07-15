9718d0ec41c6471a068efb59a40072fc313c07ae
hereiam123 <bdemaio1@gmail.com>
2019-07-14 20:41:02 -0400

    Connecting to Mongo DB

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const connectDB = require("./config/db.js");
 
 const app = express();
+
+//Connect to Mongo DB
+connectDB();
 
 //Allows JSON data
 app.use(express.json({ extended: false }));
 
 const PORT = 5000;
 
-app.listenerCount(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
