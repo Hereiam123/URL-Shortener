cc9c4ace8f976a4ef20ac810341259f39b47d026
hereiam123 <bdemaio1@gmail.com>
2019-07-14 21:14:41 -0400

    Setup for request of short url, to redirect to long version of url

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,23 @@ const express = require("express");
 
 const router = express.Router();
 
+const Url = require("../models/Url");
+
+//@Route GET /:urlCode
+//@Desc Redirect to url encoded
+router.get("/:urlCode", async (req, res) => {
+  try {
+    //See if long version of url exists in DB for short code request
+    const url = await Url.findOne({ urlCode: req.params.urlCode });
+    if (url) {
+      return res.redirect(url.longUrl);
+    } else {
+      return res.status(404).json("No url found");
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json("Server error");
+  }
+});
+
 module.exports = router;
